ed6d01e6220024359275ebe6b13ce0e13d9269c0
sindiana <sindiana9d26@gmail.com>
2022-05-25 16:18:46 +0700

    ujian

---
+++ koneksi.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ujian_sindiana_2440087031;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author somatech-devops
+ */
+public class koneksi {
+    public static Connection getConnection() {
+        Connection conn = null;
+        String url = "jdbc:mysql://localhost:3306/data_pasien";
+        String user = "root";
+        String password = "";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection c = koneksi.getConnection();
+            System.out.println(String.format("Connected to database %s " + "successfully.", c.getCatalog()));
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
